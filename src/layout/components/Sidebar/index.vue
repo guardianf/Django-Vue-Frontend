0e70786e5339fe989290a3b7c044f9cfb003ef76
guardianf <578248377@qq.com>
2021-05-14 10:01:51 +0800

    complete sidebar

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,39 +1,44 @@
 <template>
   <div :class="{'has-logo':showLogo}">
     <logo v-if="showLogo" :collapse="isCollapse" />
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu
-        :default-active="activeMenu"
-        :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :unique-opened="false"
-        :active-text-color="variables.menuActiveText"
-        :collapse-transition="false"
-        mode="vertical"
-      >
+    <el-menu
+      class="fvr-menu"
+      :default-active="activeMenu"
+      :collapse="isCollapse"
+      :background-color="variables.menuBg"
+      :text-color="variables.menuText"
+      :unique-opened="false"
+      :active-text-color="variables.menuActiveText"
+      :collapse-transition="false"
+      mode="vertical"
+    >
+      <el-scrollbar wrap-class="scrollbar-wrapper" class="span">
         <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" />
-      </el-menu>
-    </el-scrollbar>
+      </el-scrollbar>
+      <el-menu-item path="/collapse" @click="toggleSideBar">
+        <i :class="collapseIcon" />
+        <span slot="title">Collapse Sidebar</span>
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, } from 'vuex'
 import Logo from './Logo'
 import SidebarItem from './SidebarItem'
 import variables from '@/styles/variables.scss'
 
 export default {
-  components: { SidebarItem, Logo },
+  components: { SidebarItem, Logo, },
   computed: {
     ...mapGetters([
       'permission_routes',
-      'sidebar'
-    ]),
+      'sidebar',
+    ],),
     activeMenu() {
       const route = this.$route
-      const { meta, path } = route
+      const { meta, path, } = route
       // if set path, the sidebar will highlight the path you set
       if (meta.activeMenu) {
         return meta.activeMenu
@@ -48,7 +53,25 @@ export default {
     },
     isCollapse() {
       return !this.sidebar.opened
-    }
-  }
+    },
+    collapseIcon() {
+      return this.isCollapse ? 'el-icon-d-arrow-right': 'el-icon-d-arrow-left'
+    },
+  },
+  methods: {
+    toggleSideBar() {
+      this.$store.dispatch('app/toggleSideBar', { withoutAnimation: false })
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.fvr-menu {
+  display: flex;
+  flex-flow: column;
+  .span {
+    flex: 1;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -84,31 +84,6 @@ export const constantRoutes = [
     ]
   },
   {
-    path: '/documentation',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/documentation/index'),
-        name: 'Documentation',
-        meta: { title: 'Documentation', icon: 'documentation', affix: true }
-      }
-    ]
-  },
-  {
-    path: '/guide',
-    component: Layout,
-    redirect: '/guide/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/guide/index'),
-        name: 'Guide',
-        meta: { title: 'Guide', icon: 'guide', noCache: true }
-      }
-    ]
-  },
-  {
     path: '/profile',
     component: Layout,
     redirect: '/profile/index',
@@ -130,255 +105,106 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/permission',
+    path: '/1',
     component: Layout,
-    redirect: '/permission/page',
+    redirect: '/robot/index',
     alwaysShow: true, // will always show the root menu
-    name: 'Permission',
+    name: 'Robot Service',
     meta: {
-      title: 'Permission',
+      title: 'Robot',
       icon: 'lock',
       roles: ['admin', 'editor'] // you can set roles in root nav
     },
     children: [
       {
-        path: 'page',
+        path: 'arm',
         component: () => import('@/views/permission/page'),
-        name: 'PagePermission',
+        name: 'Arm',
         meta: {
-          title: 'Page Permission',
+          title: 'Arm',
           roles: ['admin'] // or you can only set roles in sub nav
         }
       },
       {
-        path: 'directive',
+        path: 'joint',
         component: () => import('@/views/permission/directive'),
-        name: 'DirectivePermission',
+        name: 'Joint',
         meta: {
-          title: 'Directive Permission'
+          title: 'Joint'
           // if do not set roles, means: this page does not require permission
         }
       },
-      {
-        path: 'role',
-        component: () => import('@/views/permission/role'),
-        name: 'RolePermission',
-        meta: {
-          title: 'Role Permission',
-          roles: ['admin']
-        }
-      }
     ]
   },
 
   {
-    path: '/icon',
+    path: '/interface',
     component: Layout,
     children: [
       {
         path: 'index',
         component: () => import('@/views/icons/index'),
-        name: 'Icons',
-        meta: { title: 'Icons', icon: 'icon', noCache: true }
+        name: 'Interface',
+        meta: { title: 'Interface', icon: 'icon', noCache: true }
       }
     ]
   },
 
   /** when your routing map is too long, you can split it into small modules **/
-  componentsRouter,
-  chartsRouter,
-  nestedRouter,
-  tableRouter,
+ // componentsRouter,
+ // chartsRouter,
+ // nestedRouter,
+ // tableRouter,
 
   {
-    path: '/example',
+    path: '/download',
     component: Layout,
     redirect: '/example/list',
-    name: 'Example',
+    name: 'download',
     meta: {
-      title: 'Example',
+      title: 'Software Service',
       icon: 'el-icon-s-help'
     },
     children: [
       {
-        path: 'create',
+        path: 'downloadcenter',
         component: () => import('@/views/example/create'),
-        name: 'CreateArticle',
-        meta: { title: 'Create Article', icon: 'edit' }
-      },
-      {
-        path: 'edit/:id(\\d+)',
-        component: () => import('@/views/example/edit'),
-        name: 'EditArticle',
-        meta: { title: 'Edit Article', noCache: true, activeMenu: '/example/list' },
-        hidden: true
-      },
-      {
-        path: 'list',
-        component: () => import('@/views/example/list'),
-        name: 'ArticleList',
-        meta: { title: 'Article List', icon: 'list' }
+        name: 'DownloadCenter',
+        meta: { title: 'Download Center', icon: 'edit' }
       }
     ]
   },
 
   {
-    path: '/tab',
+    path: '/management',
     component: Layout,
+    meta: { title: 'Management Service', icon: 'edit' },
     children: [
       {
-        path: 'index',
+        path: 'user',
         component: () => import('@/views/tab/index'),
-        name: 'Tab',
-        meta: { title: 'Tab', icon: 'tab' }
-      }
-    ]
-  },
-
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'Error Pages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: () => import('@/views/error-page/401'),
-        name: 'Page401',
-        meta: { title: '401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: '404', noCache: true }
-      }
-    ]
-  },
-
-  {
-    path: '/error-log',
-    component: Layout,
-    children: [
-      {
-        path: 'log',
-        component: () => import('@/views/error-log/index'),
-        name: 'ErrorLog',
-        meta: { title: 'Error Log', icon: 'bug' }
-      }
-    ]
-  },
-
-  {
-    path: '/excel',
-    component: Layout,
-    redirect: '/excel/export-excel',
-    name: 'Excel',
-    meta: {
-      title: 'Excel',
-      icon: 'excel'
-    },
-    children: [
-      {
-        path: 'export-excel',
-        component: () => import('@/views/excel/export-excel'),
-        name: 'ExportExcel',
-        meta: { title: 'Export Excel' }
-      },
-      {
-        path: 'export-selected-excel',
-        component: () => import('@/views/excel/select-excel'),
-        name: 'SelectExcel',
-        meta: { title: 'Export Selected' }
-      },
-      {
-        path: 'export-merge-header',
-        component: () => import('@/views/excel/merge-header'),
-        name: 'MergeHeader',
-        meta: { title: 'Merge Header' }
-      },
-      {
-        path: 'upload-excel',
-        component: () => import('@/views/excel/upload-excel'),
-        name: 'UploadExcel',
-        meta: { title: 'Upload Excel' }
-      }
-    ]
-  },
-
-  {
-    path: '/zip',
-    component: Layout,
-    redirect: '/zip/download',
-    alwaysShow: true,
-    name: 'Zip',
-    meta: { title: 'Zip', icon: 'zip' },
-    children: [
-      {
-        path: 'download',
-        component: () => import('@/views/zip/index'),
-        name: 'ExportZip',
-        meta: { title: 'Export Zip' }
-      }
-    ]
-  },
-
-  {
-    path: '/pdf',
-    component: Layout,
-    redirect: '/pdf/index',
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/pdf/index'),
-        name: 'PDF',
-        meta: { title: 'PDF', icon: 'pdf' }
-      }
-    ]
-  },
-  {
-    path: '/pdf/download',
-    component: () => import('@/views/pdf/download'),
-    hidden: true
-  },
-
-  {
-    path: '/theme',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/theme/index'),
-        name: 'Theme',
-        meta: { title: 'Theme', icon: 'theme' }
-      }
-    ]
-  },
-
-  {
-    path: '/clipboard',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/clipboard/index'),
-        name: 'ClipboardDemo',
-        meta: { title: 'Clipboard', icon: 'clipboard' }
-      }
-    ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://github.com/PanJiaChen/vue-element-admin',
-        meta: { title: 'External Link', icon: 'link' }
+        name: 'User',
+        meta: { title: 'User', icon: 'tab' }
+      }, {
+        path: 'role',
+        component: () => import('@/views/tab/index'),
+        name: 'Role',
+        meta: { title: 'Role', icon: 'tab' }
+      }, {
+        path: 'permission',
+        component: () => import('@/views/tab/index'),
+        name: 'Permission',
+        meta: { title: 'Permission', icon: 'tab' }
+      }, {
+        path: 'group',
+        component: () => import('@/views/tab/index'),
+        name: 'Group',
+        meta: { title: 'Group', icon: 'tab' }
+      }, {
+        path: 'menu',
+        component: () => import('@/views/tab/index'),
+        name: 'Menu',
+        meta: { title: 'Menu', icon: 'tab' }
       }
     ]
   },
